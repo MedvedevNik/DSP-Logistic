e248d17fa464736e1e065c330593595bf40f77b5
Nik Medvedev <MedvedevKlaus@yandex.ru>
2020-12-02 05:39:26 +0300

    feat: create new project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// const header = document.getElementById("header"),
+//       sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     header.classList.toggle("sticky");
+//   } else {
+//     header.classList.toggle("sticky");
+//   }
+// }
+
+// window.onscroll = function() {myFunction()};
