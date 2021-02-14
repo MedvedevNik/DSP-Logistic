c9baa927675d34e38e82a42b88a7cdb41956297a
Nik Medvedev <MedvedevKlaus@yandex.ru>
2021-02-14 18:19:17 +0300

    fix: fixed mobile-menu

--- telegram.php
+++ telegram.php
@@ -20,6 +20,6 @@ foreach ($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if(!$sendToTelegram) {
-  echo 'Error';
+  echo 'hi';
 }
 ?>
